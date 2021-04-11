e0f4cf4b5f47e6542f80c29dea9ac49c2482dee8
Andy Schachter <andy.schachter@gmail.com>
2021-04-11 16:41:23 -0400

    pushing files back up to check errors

---
+++ orderBook.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+const reconcileOrder = (existingBook, incomingOrder) => {
+  let updatedBook = []
+
+  if (existingBook.length === 0) {
+    updatedBook.push(incomingOrder)
+  }
+
+  if (existingBook.type === incomingOrder.type) {
+    updatedBook.push(incomingOrder)
+  }
+
+  return updatedBook
+}
+
+console.log(reconcileOrder([{ type: 'sell', quantity: 10, price: 6150 }], { type: 'sell', quantity: 12, price: 6000 }))
+
+module.exports = reconcileOrder
