5e2c446cdb13d120b34c8622613d94f1710a870c
Andy Schachter <andy.schachter@gmail.com>
2021-04-11 23:14:41 -0400

    added if statements for non matching corresponding order types

--- orderBook.js
+++ orderBook.js
@@ -5,14 +5,28 @@ const reconcileOrder = (existingBook, incomingOrder) => {
   if (existingBook.length === 0) {
     updatedBook.push(incomingOrder)
   }
-  
-  if (existingBook.length > 0 && existingBook[0].type === incomingOrder.type) {
-    updatedBook.push(existingBook[0], incomingOrder)
+
+  if (existingBook.length > 0) {
+    for (let i=0; i < existingBook.length; i++) {
+      if (existingBook[i].type === incomingOrder.type) {
+        updatedBook.push(existingBook[i], incomingOrder)        
+      }
+      if (existingBook[i].type !== incomingOrder.type && existingBook[i].quantity === incomingOrder.quantity && existingBook[i].price === incomingOrder.price) {
+        i++
+      }      
+      if (existingBook[i].type !== incomingOrder.type && existingBook[i].quantity !== incomingOrder.quantity && existingBook[i].price !== incomingOrder.price) {
+        updatedBook.push(existingBook[i], incomingOrder)  
+      }
+
+
+    }
   }
 
   return updatedBook
 }
 
 console.log(reconcileOrder([{ type: 'sell', quantity: 10, price: 6150 }], { type: 'sell', quantity: 12, price: 6000 }))
+let object = { type: 'sell', quantity: 12, price: 6000 }
+console.log(object)
 
 module.exports = reconcileOrder
